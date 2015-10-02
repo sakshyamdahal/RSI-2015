f5dd1a13ed8426432c9fc6c69871ef5c4e1d5cc0
Sakshyam Dahal <dahalsakshyam@gmail.com>
2015-10-02 15:24:29 -0400

    add user

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_member!
 
   # GET /jobs
   # GET /jobs.json
